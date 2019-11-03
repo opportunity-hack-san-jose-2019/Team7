6b41ba69c7471280faf1b73edf7693161385a7c4
Kidambi,Krishna Vamsi(kkidambi) <kkidambi@paypal.com>
2019-11-02 18:30:18 -0700

    Add task request

--- Backend/src/main/java/com/example/demo/api/ActionResource.java
+++ Backend/src/main/java/com/example/demo/api/ActionResource.java
@@ -3,6 +3,7 @@ package com.example.demo.api;
 import javax.ws.rs.core.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.TaskRequest;
 import com.example.demo.model.UserProfile;
 
 
@@ -12,6 +13,8 @@ public class ActionResource {
 
     @RequestMapping("/findhelp")
     Response addTask(TaskRequest taskRequest) {
+
+
         // TODO: this API adds task to DB
         return Response.ok().build();
     }
---
+++ Backend/src/main/java/com/example/demo/model/TaskRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+
+public class TaskRequest {
+
+    RequestCategoryEnum requestCategory;
+    String timeStampForEvent;
+    String creationTime;
+    String purposeOfRequest;
+    String descriptionOfRequest;
+    String eventCoordinates;
+    String addressLine1;
+    String addressLine2;
+    String city;
+    String State;
+    String Country;
+    String zipCode;
+    String requestorId;
+    String requestorName;
+
+}
